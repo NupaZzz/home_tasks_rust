1d2aae41cf42857ba73f867a8540c4079922562d
NupaZzz <Nupalolz@gmail.com>
2025-03-15 15:29:08 +0300

    Добавлена функция деления двух чисел с обработкой ошибок, включая проверку на деление на ноль. Обновлено меню выбора заданий для включения нового задания по делению.

--- task_3/src/main.rs
+++ task_3/src/main.rs
@@ -65,7 +65,31 @@ fn join_strings() {
     println!("{}", s);
 }
 
+fn div() -> Result<f64, String> {
+    print!("Введите число один: ");
+    let a: f64 = user_input()
+        .trim()
+        .parse::<f64>()
+        .map_err(|err| err.to_string())?;
+
+    print!("Введите число два: ");
+    let b: f64 = user_input()
+        .trim()
+        .parse::<f64>()
+        .map_err(|err| err.to_string())?;
+
+    if b == 0.0 {
+        return Err("Деление на ноль!".to_string());
+    }
+
+    Ok(a / b)
+}
+
 fn main() {
+    println!("1. Задание 1: Ввод пяти чисел для вектора");
+    println!("2. Задание 2: Ввод имени студента и вывод оценки");
+    println!("3. Задание 3: Сложение двух строк");
+    println!("4. Задание 4: Деление двух чисел");
     print!("Выберите задание: ");
     match user_input().trim().parse::<i32>() {
         Ok(input) => {
@@ -84,6 +108,14 @@ fn main() {
                 },
                 4 => {
                     println!("Вы выбрали задание: {input}");
+                    match div() {
+                        Ok(result) => {
+                            println!("Результат: {result}");
+                        }
+                        Err(err) => {
+                            println!("Ошибка: {err}");
+                        }
+                    }
                 },
                 _ => {
                     println!("Ошибка: Данного задания не существует!");
